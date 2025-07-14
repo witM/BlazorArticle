905953db1bd719ccde39331b6c91e256a308b71d
Marian Witek <witekniezajete@gmail.com>
2025-07-14 13:48:51 +0200

    helper utils

---
+++ BlazorArticle/Utils/ActionAsync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorArticle.Utils
+{
+    /// <summary>
+    /// Delegate which representing asynchronous action.
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    /// <param name="model">TModel object.</param>
+    /// <returns>returns a Task </returns>
+    public delegate Task ActionAsync<TModel>(TModel model);
+
+}
